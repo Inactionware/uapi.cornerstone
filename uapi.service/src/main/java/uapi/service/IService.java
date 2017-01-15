df5d1dc7e06d7974f4288b199eb195b927bee9aa
minjing <min_jing@aliyun.com>
2017-01-15 18:02:39 +0800

    #1 Add java file to ensure test coverage work correct

---
+++ uapi.service/src/main/java/uapi/service/IService.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2010 The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.service;
+
+//import uapi.Type;
+
+/**
+ * Implement this interface will indicate the object is managed by framework
+ */
+public interface IService {
+
+    String METHOD_GETIDS                        = "getIds";
+    String METHOD_GET_DEPENDENCIES              = "getDependencies";
+//    String METHOD_GETIDS_RETURN_TYPE            = Type.STRING_ARRAY;
+//    String METHOD_GET_DEPENDENT_ID_RETURN_TYPE  = Type.STRING_ARRAY;
+
+    /**
+     * Return the service identifications
+     *
+     * @return  The service identifications
+     */
+    String[] getIds();
+}
