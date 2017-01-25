becb207850c525077d675a88ce7710ef74686aa8
minjing <min_jing@aliyun.com>
2017-01-25 15:09:10 +0800

    #5 Add new project for log framework

---
+++ uapi.log/src/main/java/uapi/log/internal/Logger.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.log.internal;
+
+import uapi.common.StringHelper;
+import uapi.log.ILogger;
+
+final class Logger implements ILogger {
+
+    private final org.slf4j.Logger _slfLogger;
+
+    Logger(org.slf4j.Logger slfLogger) {
+        this._slfLogger = slfLogger;
+    }
+
+    @Override
+    public void trace(String message, Object... parameters) {
+        this._slfLogger.trace(message, parameters);
+    }
+
+    @Override
+    public void debug(String message, Object... parameters) {
+        this._slfLogger.debug(message, parameters);
+    }
+
+    @Override
+    public void info(String message, Object... parameters) {
+        this._slfLogger.info(message, parameters);
+    }
+
+    @Override
+    public void warn(String message, Object... parameters) {
+        this._slfLogger.warn(message, parameters);
+    }
+
+    @Override
+    public void warn(Throwable t) {
+        this._slfLogger.warn(t.getMessage(), t);
+    }
+
+    @Override
+    public void warn(Throwable t, String message, Object... parameters) {
+        this._slfLogger.warn(StringHelper.makeString(message, parameters), t);
+    }
+
+    @Override
+    public void error(String message, Object... parameters) {
+        this._slfLogger.error(message, parameters);
+    }
+
+    @Override
+    public void error(Throwable t) {
+        this._slfLogger.error(t.getMessage(), t);
+    }
+
+    @Override
+    public void error(Throwable t, String message, Object... parameters) {
+        this._slfLogger.error(StringHelper.makeString(message, parameters), t);
+    }
+}
---
+++ uapi.log/src/main/java/uapi/log/internal/LoggerManager.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2017. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product
+ */
+
+package uapi.log.internal;
+
+import org.slf4j.LoggerFactory;
+import uapi.InvalidArgumentException;
+import uapi.log.ILogger;
+import uapi.service.IServiceFactory;
+import uapi.service.annotation.Service;
+import uapi.service.annotation.Tag;
+
+@Service
+@Tag("Log")
+public class LoggerManager implements IServiceFactory<ILogger> {
+
+    @Override
+    public ILogger createService(Object serveFor) {
+        if (serveFor == null) {
+            throw new InvalidArgumentException("servFor", InvalidArgumentException.InvalidArgumentType.EMPTY);
+        }
+        return new Logger(LoggerFactory.getLogger(serveFor.getClass()));
+    }
+}
