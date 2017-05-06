d4f02d8dcc37ca689ca6f5bf6905e37e9dc1faca
minjing <min_jing@aliyun.com>
2017-05-06 14:43:20 +0800

    #31 Add unit test for OnInjectParser

---
+++ uapi.service/src/test/groovy/uapi/service/internal/OnInjectParserTest.groovy
@@ -0,0 +1,310 @@
+package uapi.service.internal
+
+import freemarker.template.Template
+import spock.lang.Specification
+import uapi.GeneralException
+import uapi.Type
+import uapi.codegen.ClassMeta
+import uapi.codegen.IBuilderContext
+import uapi.codegen.LogSupport
+import uapi.codegen.MethodMeta
+import uapi.codegen.ParameterMeta
+import uapi.service.IServiceLifecycle
+import uapi.service.annotation.OnInject
+
+import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.Name
+import javax.lang.model.element.VariableElement
+import javax.lang.model.type.TypeMirror
+
+/**
+ * Unit tests for OnInjectParser
+ */
+class OnInjectParserTest extends Specification {
+
+    def 'Test create instance'() {
+        when:
+        def parser = new OnInjectParser()
+
+        then:
+        noExceptionThrown()
+        parser != null
+        parser.helper != null
+    }
+
+    def 'Test parser on incorrect element type'() {
+        given:
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                1 * info(_ as String)
+            }
+        }
+        def element = Mock(Element) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'methodElement'
+            }
+            getKind() >> elementKind
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.parse(builderCtx, [element] as Set)
+
+        then:
+        thrown(GeneralException)
+
+        where:
+        elementKind             | placeholder
+        ElementKind.CLASS       | null
+        ElementKind.FIELD       | null
+        ElementKind.ENUM        | null
+        ElementKind.CONSTRUCTOR | null
+    }
+
+    def 'Test parser on incorrect method return type'() {
+        given:
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                1 * info(_ as String)
+            }
+        }
+        def element = Mock(ExecutableElement) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'method'
+            }
+            getKind() >> ElementKind.METHOD
+            getReturnType() >> Mock(TypeMirror) {
+                toString() >> rtnType
+            }
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.parse(builderCtx, [element] as Set)
+
+        then:
+        thrown(GeneralException)
+
+        where:
+        rtnType             | placeholder
+        Type.Q_ARRAY_LIST   | null
+        Type.INTEGER        | null
+        Type.Q_STRING       | null
+    }
+
+    def 'Test parser on incorrect method parameter count'() {
+        given:
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                1 * info(_ as String)
+            }
+        }
+        def element = Mock(ExecutableElement) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'method'
+            }
+            getKind() >> ElementKind.METHOD
+            getReturnType() >> Mock(TypeMirror) {
+                toString() >> Type.VOID
+            }
+            getParameters() >> params
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.parse(builderCtx, [element] as Set)
+
+        then:
+        thrown(GeneralException)
+
+        where:
+        params                                          | placeholder
+        []                                              | null
+        [Mock(VariableElement), Mock(VariableElement)]  | null
+    }
+
+    def 'Test parser'() {
+        given:
+        def tempInjectModel = [:]
+        def classElement = Mock(Element)
+        def classBuilder = Mock(ClassMeta.Builder) {
+            createTransienceIfAbsent(OnInjectParser.MODEL_ON_INJECT, _) >> tempInjectModel
+        }
+        1 * classBuilder.findMethodBuilders(_) >> []
+        1 * classBuilder.addImplement(IServiceLifecycle.class.canonicalName) >> classBuilder
+        1 * classBuilder.addMethodBuilder(_) >> classBuilder
+        1 * classBuilder.getTransience(OnInjectParser.MODEL_ON_INJECT) >> tempInjectModel
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                1 * info(_ as String)
+            }
+            findClassBuilder(classElement) >> classBuilder
+            1 * loadTemplate(_) >> Mock(Template)
+        }
+        def element = Mock(ExecutableElement) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'method'
+            }
+            getEnclosingElement() >> classElement
+            getKind() >> ElementKind.METHOD
+            getReturnType() >> Mock(TypeMirror) {
+                toString() >> Type.VOID
+            }
+            getParameters() >> [Mock(VariableElement) {
+                asType() >> Mock(TypeMirror) {
+                    toString() >> paramType
+                }
+            }]
+            1 * getAnnotation(_) >> Test.getMethod('test', String.class).getAnnotation(OnInject.class)
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.parse(builderCtx, [element] as Set)
+
+        then:
+        noExceptionThrown()
+        tempInjectModel.size() == 1
+        List methods = tempInjectModel.get(OnInjectParser.VAR_METHODS)
+        methods.size() == 1
+        Map method = methods.get(0)
+        method[OnInjectParser.VAR_METHOD_NAME] == 'method'
+        method[OnInjectParser.VAR_SVC_ID] == paramType
+        method[OnInjectParser.VAR_SVC_TYPE] == paramType
+
+        where:
+        paramType       | placeholder
+        'String'        | null
+    }
+
+    def 'Test parser on existing builder'() {
+        given:
+        def tempInjectModel = [:]
+        def classElement = Mock(Element)
+        def classBuilder = Mock(ClassMeta.Builder) {
+            createTransienceIfAbsent(OnInjectParser.MODEL_ON_INJECT, _) >> tempInjectModel
+        }
+        1 * classBuilder.findMethodBuilders(_) >> [Mock(MethodMeta.Builder) {
+            getReturnTypeName() >> Type.VOID
+            getParameterCount() >> 2
+            findParameterBuilder(OnInjectParser.PARAM_SVC_ID) >> Mock(ParameterMeta.Builder) {
+                1 * getType() >> Type.Q_STRING
+            }
+            findParameterBuilder(OnInjectParser.PARAM_SVC) >> Mock(ParameterMeta.Builder) {
+                1 * getType() >> Type.Q_OBJECT
+            }
+        }]
+        0 * classBuilder.addImplement(IServiceLifecycle.class.canonicalName) >> classBuilder
+        0 * classBuilder.addMethodBuilder(_) >> classBuilder
+        0 * classBuilder.getTransience(OnInjectParser.MODEL_ON_INJECT) >> tempInjectModel
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                1 * info(_ as String)
+            }
+            findClassBuilder(classElement) >> classBuilder
+            0 * loadTemplate(_) >> Mock(Template)
+        }
+        def element = Mock(ExecutableElement) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'method'
+            }
+            getEnclosingElement() >> classElement
+            getKind() >> ElementKind.METHOD
+            getReturnType() >> Mock(TypeMirror) {
+                toString() >> Type.VOID
+            }
+            getParameters() >> [Mock(VariableElement) {
+                asType() >> Mock(TypeMirror) {
+                    toString() >> paramType
+                }
+            }]
+            1 * getAnnotation(_) >> Test.getMethod('test', String.class).getAnnotation(OnInject.class)
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.parse(builderCtx, [element] as Set)
+
+        then:
+        noExceptionThrown()
+        tempInjectModel.size() == 1
+        List methods = tempInjectModel.get(OnInjectParser.VAR_METHODS)
+        methods.size() == 1
+        Map method = methods.get(0)
+        method[OnInjectParser.VAR_METHOD_NAME] == 'method'
+        method[OnInjectParser.VAR_SVC_ID] == paramType
+        method[OnInjectParser.VAR_SVC_TYPE] == paramType
+
+        where:
+        paramType       | placeholder
+        'String'        | null
+    }
+
+
+    def 'Test add inject method if absent'() {
+        given:
+        def tempInjectModel = [:]
+        def classElement = Mock(Element)
+        def classBuilder = Mock(ClassMeta.Builder) {
+            createTransienceIfAbsent(OnInjectParser.MODEL_ON_INJECT, _) >> tempInjectModel
+        }
+        1 * classBuilder.findMethodBuilders(_) >> [Mock(MethodMeta.Builder) {
+            getReturnTypeName() >> Type.VOID
+            getParameterCount() >> 2
+            findParameterBuilder(OnInjectParser.PARAM_SVC_ID) >> Mock(ParameterMeta.Builder) {
+                1 * getType() >> Type.Q_STRING
+            }
+            findParameterBuilder(OnInjectParser.PARAM_SVC) >> Mock(ParameterMeta.Builder) {
+                1 * getType() >> Type.Q_OBJECT
+            }
+        }]
+        0 * classBuilder.addImplement(IServiceLifecycle.class.canonicalName) >> classBuilder
+        0 * classBuilder.addMethodBuilder(_) >> classBuilder
+        0 * classBuilder.getTransience(OnInjectParser.MODEL_ON_INJECT) >> tempInjectModel
+        def builderCtx = Mock(IBuilderContext) {
+            getLogger() >> Mock(LogSupport) {
+                0 * info(_ as String)
+            }
+            findClassBuilder(classElement) >> classBuilder
+            0 * loadTemplate(_) >> Mock(Template)
+        }
+        def element = Mock(ExecutableElement) {
+            getSimpleName() >> Mock(Name) {
+                toString() >> 'method'
+            }
+            getEnclosingElement() >> classElement
+            getKind() >> ElementKind.METHOD
+            getReturnType() >> Mock(TypeMirror) {
+                toString() >> Type.VOID
+            }
+            getParameters() >> [Mock(VariableElement) {
+                asType() >> Mock(TypeMirror) {
+                    toString() >> paramType
+                }
+            }]
+            0 * getAnnotation(_) >> Test.getMethod('test', String.class).getAnnotation(OnInject.class)
+        }
+        def parser = new OnInjectParser()
+
+        when:
+        parser.addInjectMethodIfAbsent(builderCtx, [element] as Set)
+
+        then:
+        noExceptionThrown()
+        tempInjectModel.size() == 1
+        List methods = tempInjectModel.get(OnInjectParser.VAR_METHODS)
+        methods.size() == 0
+
+        where:
+        paramType       | placeholder
+        'String'        | null
+    }
+
+    class Test {
+
+        @OnInject
+        void test(String str) {}
+    }
+}
