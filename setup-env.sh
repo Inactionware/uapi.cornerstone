67732c38a13f28acefeac24b52b2a03640f4b3f3
minjing <min_jing@aliyun.com>
2019-02-22 16:50:12 +0800

    #101 Using common script to init dev env'

--- setup-env.sh
+++ setup-env.sh
@@ -3,69 +3,36 @@
 # The configuration repo branch/tag name which will be checked out
 cfgBranch=master
 
-# The required java version for build environment
-jversion=1.8
+rm -rf .config
+mkdir .config
+cd .config
 
-# Define java home for different development environment
-jhome_mac="/Library/Java/JavaVirtualMachines/jdk1.8.0_181.jdk/Contents/Home"
-jhome_vm="/home/min/Dev/zuluJdk-8.0.192"
+git init
+git remote add -f origin https://gitlab.com/Inactionware/configuration.git
+git config core.sparsecheckout true
+echo "uapi" >> .git/info/sparse-checkout
+git checkout ${cfgBranch}
 
-os=`uname -s`
-host=`hostname`
-cmd_javac=`command -v javac`
-ver_java=0
-msg_err=""
+cd ..
 
-# Check java version
-if [ -n "$cmd_javac" ]
-then
-    echo -e "Java environment is detected...\c"
-    ver_java=`$cmd_javac -version 2>&1`
-    echo -e "version is $ver_java\n"
-    ver_java=`expr substr "$ver_java" 7 3`
-    if [ "$?" != "0" ]
-    then
-        msg_err="Run expr command error!"
-    fi
-fi
+# Initialize development evnironment
+source .config/uapi/setup-dev.sh
+#! /bin/bash
 
-# Setup java development envirnoment
-if [ -z "$msg_err" ] && [ "$ver_java" != "$jversion" ]
-then
-    echo "No Java environment found or the version of Java is not supported."
-    echo -e "Initialize JAVA environment......\c"
-    if [ $USER == "min" ] && [ $host == "min-vm-elementary" ]       # At Linux VM host development
-    then
-        export JAVA_HOME=$jhome_vm
-    elif [ $USER == "xiaoming" ] && [ $host == "min-home.local" ]   # At MacOS host development
-    then
-        export JAVA_HOME=$jhome_mac
-    else
-        msg_err="\nUnsupported user - $USER@$host, please reconfig the user"
-    fi
-    export PATH=$JAVA_HOME/bin:$PATH
-    echo "Done"
-fi
+# The configuration repo branch/tag name which will be checked out
+cfgBranch=master
 
-# Check out build configuration repo from remote
-if [ -z "$msg_err" ]
-then
-    rm -rf .config
-    mkdir .config
-    cd .config
+rm -rf .config
+mkdir .config
+cd .config
 
-    git init
-    git remote add -f origin https://gitlab.com/Inactionware/configuration.git
-    git config core.sparsecheckout true
-    echo "uapi" >> .git/info/sparse-checkout
-    git checkout ${cfgBranch}
+git init
+git remote add -f origin https://gitlab.com/Inactionware/configuration.git
+git config core.sparsecheckout true
+echo "uapi" >> .git/info/sparse-checkout
+git checkout ${cfgBranch}
 
-    # Run gradle build script
-    cd ..
-    # ./gradlew clean build
-fi
+cd ..
 
-if [ -n "$msg_err" ]
-then
-    echo -e "$msg_err"
-fi
+# Initialize development evnironment
+source .config/uapi/setup-dev.sh
