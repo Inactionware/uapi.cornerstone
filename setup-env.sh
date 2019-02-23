d46f65d584877973807712798014316b43669d4b
minjing <min_jing@aliyun.com>
2019-02-23 11:41:09 +0800

    #101 fix issue

--- setup-env.sh
+++ setup-env.sh
@@ -17,23 +17,3 @@ cd ..
 
 # Initialize development evnironment
 source .config/uapi/setup-dev.sh
-#! /bin/bash
-
-# The configuration repo branch/tag name which will be checked out
-cfgBranch=master
-
-rm -rf .config
-mkdir .config
-cd .config
-
-git init
-git remote add -f origin https://gitlab.com/Inactionware/configuration.git
-git config core.sparsecheckout true
-echo "uapi" >> .git/info/sparse-checkout
-git checkout ${cfgBranch}
-
-cd ..
-
-# Initialize development evnironment
-source .config/uapi/setup-dev.sh
- 
