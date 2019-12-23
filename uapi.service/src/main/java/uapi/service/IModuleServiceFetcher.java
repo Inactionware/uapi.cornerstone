e62464b51bbf8e8c8f9f878d2ef040bafbf71c8f
minjing <min_jing@aliyun.com>
2019-12-23 20:53:55 +0800

    Add interface to fetch all generated services

---
+++ uapi.service/src/main/java/uapi/service/IModuleServiceFetcher.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019. The UAPI Authors
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at the LICENSE file.
+ *
+ * You must gained the permission from the authors if you want to
+ * use the project into a commercial product.
+ */
+
+package uapi.service;
+
+/**
+ * The interface is used to fetch services from current module.
+ */
+public interface IModuleServiceFetcher {
+
+    /**
+     * Fetch all service from this module
+     *
+     * @return  All services
+     */
+    IService[] fetch();
+}
