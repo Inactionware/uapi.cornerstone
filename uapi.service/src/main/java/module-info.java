eaaa6a5eb3c4896d6b51ebf3640ad06eae52df44
minjing <min_jing@aliyun.com>
2019-12-24 22:01:16 +0800

    Using common subproject script block in the root build file

--- uapi.service/src/main/java/module-info.java
+++ uapi.service/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module uapi.service {
     requires auto.service.annotations;
     requires freemarker;
     requires uapi.common;
+    requires uapi.codegen;
     requires uapi.annotation;
     requires uapi.exception;
     requires uapi.state;
